c1ede4ed274edf4714e6684b1e4e837f7251ae6a
Winicius Pichirilo <91923386+WiniciusPichirilo@users.noreply.github.com>
2024-10-16 21:30:30 -0300

    Update Moedas.java

--- src/Moedas.java
+++ src/Moedas.java
@@ -1,10 +1,52 @@
-public class Conversor {
-    public static void main(String[] args) {
-        Moedas moedas = new Moedas();
-        double usdToBrl = moedas.obterTaxaDeCambio("BRL");
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.util.Set;
 
-        if (usdToBrl != -1) {
-            System.out.println("1 USD = " + usdToBrl + " BRL");
+public class Moedas {
+    private static final String API_KEY = "4da70373bd7d81457f51905e"; // Sua chave de API
+    private static final String API_URL = "https://api.exchangerate-api.com/v4/latest/USD?access_key=" + API_KEY;
+
+    public JsonObject obterTodasAsTaxas() {
+        try {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder().url(API_URL).build();
+            Response response = client.newCall(request).execute();
+
+            if (response.isSuccessful()) {
+                String jsonData = response.body().string();
+                JsonObject jsonObject = JsonParser.parseString(jsonData).getAsJsonObject();
+                return jsonObject.getAsJsonObject("rates");
+            } else {
+                System.out.println("Erro na requisição da API");
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public double obterTaxaDeCambio(String moedaDestino) {
+        JsonObject rates = obterTodasAsTaxas();
+        if (rates != null && rates.has(moedaDestino)) {
+            return rates.get(moedaDestino).getAsDouble();
+        } else {
+            System.out.println("Moeda não encontrada.");
+            return -1;
+        }
+    }
+
+    public void listarMoedas() {
+        JsonObject rates = obterTodasAsTaxas();
+        if (rates != null) {
+            Set<String> moedas = rates.keySet();
+            System.out.println("Moedas disponíveis para conversão:");
+            for (String moeda : moedas) {
+                System.out.println(moeda);
+            }
         }
     }
 }
